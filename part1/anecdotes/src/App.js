06b762cef9bf79ecca8577f5d76c2a9375b325b9
TanweerAlam <tanweeralam1312@gmail.com>
2022-11-11 16:42:04 +0530

    Part1-anecdotes ex-1.12 to ex-1.14 completed

---
+++ part1/anecdotes/src/App.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+
+const Button = ({ handleClick, buttonText }) => {
+  return <button onClick={handleClick}>{buttonText}</button>;
+};
+
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+  ];
+
+  const [selected, setSelected] = useState(0);
+  const [points, setPoints] = useState(new Array(anecdotes.length).fill(0));
+
+  const randomGenerator = (len) => {
+    let randomNum = Math.floor(Math.random() * len);
+    return randomNum;
+  };
+
+  const showAnecdote = (arr) => {
+    let arrLength = arr.length;
+    const index = randomGenerator(arrLength);
+    setSelected(index);
+  };
+
+  const vote = () => {
+    const copy = [...points];
+    copy[selected] += 1;
+    setPoints(copy);
+  };
+
+  const highestVoted = () => {
+    const index = points.indexOf(Math.max(...points));
+    return index;
+  };
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>has {points[selected]} votes</p>
+      <Button handleClick={vote} buttonText="vote" />
+      <Button
+        handleClick={() => showAnecdote(anecdotes)}
+        buttonText="next anecdote"
+      />
+      {!points.every((elem) => elem === 0) && (
+        <>
+          <h1>Anecdote with most votes</h1>
+          {anecdotes[highestVoted()]}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default App;
